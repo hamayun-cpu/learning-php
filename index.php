e9a1ec0d9e46b65e9d8fed8cd3ca261aa178bc3d
hamayun waheed <hamayun@hamayuns-MacBook-Pro.local>
2021-12-23 05:07:15 +0500

    understand scope and hoisting

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-$animals = ['dog', 'cat', 'mouse'];
+// $animals = ['dog', 'cat', 'mouse'];
 
-require 'index.view.php';
+// require 'index.view.php';
+
+
+function calculate_count() {
+	global $count;
+	// will print 0 and increment global variable
+    echo $count . ' ';
+	$count++; 
+    echo $count . ' ';
+}
+
+$count = 10;
+calculate_count();
+echo $count;
