c5e7058ef6e1280dbd1c53dd59d7c51294320dd0
hamayun waheed <hamayun@hamayuns-MacBook-Pro.local>
2021-12-28 01:17:34 +0500

    learn classes

--- index.php
+++ index.php
@@ -1,5 +1,43 @@
 <?php
 
+require 'function.php';
+
+
+class Task {
+    public $description;
+
+    protected $status;
+
+    public function __construct($desc, $status) {
+        $this->description = $desc;
+        $this->status = $status;
+    }
+
+    public function complete() {
+        $this->status = true;
+    }
+
+    public function isComplete() {
+        return $this->status;
+    }
+}
+
+$tasks = [
+    new Task('shave', true),
+    new Task('brush', false),
+    new Task('take shower', false)
+];
+
+require 'index.view.php';
+
+// dd($tasks);
+
+// dd($tasks);
+
+
+
+// require 'index.view.php';
+
 // $animals = ['dog', 'cat', 'mouse'];
 
 // require 'index.view.php';
@@ -15,29 +53,28 @@
 
 // function counter()
 // {
-//     global $count;
-//     $count++;
+//     $count = 1;
 //     function counte()
 //     {
 //         global $count;
-//         echo $count . ' ';
+//         echo $count . 'qwe ';
 //         $count++;
 //     }
 //     counte();
-//     echo $count;
+//     echo $count . ' ';
 // }
+
 // $count = 1;
 // counter();
 
 
-$a = [1,2,3,4];
-$b = $a;
-$b[0]=0;
-$b="hello";
-// $a = 'hello';
+// $a = [1,2,3,4];
+// $b = $a;
+// $b[0]=0;
+// $b="hello";
+// // $a = 'hello';
 
-print_r($a);
-print_r($b);
+// print_r($a);
+// print_r($b);
 // echo $a[1];
 // echo $b;
-
--- index.view.php
+++ index.view.php
@@ -2,17 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <ul>
-        <?php
-            foreach($animals as $animal) {
-                echo "<li>$animal</li>";
-            }
-        ?>
+        <?php foreach ($tasks as $task) : ?>
+            
+            <li>
+                <?= $task->description; ?>
+                <?php 
+                    if($task->isComplete()) {
+                        echo "Completed";
+                    }
+                ?>
+            </li>
+            
+        <?php endforeach; ?>
     </ul>
 </body>
 </html>
