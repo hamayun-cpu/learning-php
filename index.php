e1aaa7a81aa537c84f7979e25f4ddae94c6d0c43
hamayun waheed <hamayun@hamayuns-MacBook-Pro.local>
2021-12-31 00:11:08 +0500

    learn dynamic pdo insertions

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require 'core/bootstrap.php';
 require Router::load('routes.php')
     ->direct(Request::uri(), Request::method());
 
+    
 // class Post {
 //     public $title;
 //     public $published;
