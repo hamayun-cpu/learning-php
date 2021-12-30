f07f815cdb542be81a6781cab5917f98e7c237f6
hamayun waheed <hamayun@hamayuns-MacBook-Pro.local>
2021-12-30 19:34:48 +0500

    learn array_filtering

--- index.php
+++ index.php
@@ -1,88 +1,41 @@
 <?php
 
 
-require 'core/bootstrap.php';
+// require 'core/bootstrap.php';
 
-require Router::load('routes.php')
-    ->direct(Request::uri());
-
- 
-
-// require 'function.php';
-
-
-// class Task {
-//     public $description;
-
-//     protected $status;
-
-//     public function __construct($desc, $status) {
-//         $this->description = $desc;
-//         $this->status = $status;
-//     }
-
-//     public function complete() {
-//         $this->status = true;
-//     }
-
-//     public function isComplete() {
-//         return $this->status;
-//     }
-// }
-
-// $tasks = [
-//     new Task('shave', true),
-//     new Task('brush', false),
-//     new Task('take shower', false)
-// ];
-
-// require 'index.view.php';
-
-// dd($tasks);
-
-// dd($tasks);
+// require Router::load('routes.php')
+//     ->direct(Request::uri());
 
 
 
-// require 'index.view.php';
+class Post {
+    public $title;
+    public $published;
 
-// $animals = ['dog', 'cat', 'mouse'];
-
-// require 'index.view.php';
-
-
-// function calculate_count() {
-// 	global $count;
-// 	// will print 0 and increment global variable
-//     echo $count . ' ';
-// 	$count++; 
-//     echo $count . ' ';
-// }
-
-// function counter()
-// {
-//     $count = 1;
-//     function counte()
-//     {
-//         global $count;
-//         echo $count . 'qwe ';
-//         $count++;
-//     }
-//     counte();
-//     echo $count . ' ';
-// }
-
-// $count = 1;
-// counter();
+    public function __construct($title, $published) {
+        $this->title = $title;
+        $this->published = $published;
+    }
+} 
 
 
-// $a = [1,2,3,4];
-// $b = $a;
-// $b[0]=0;
-// $b="hello";
-// // $a = 'hello';
+$posts = [
+    new Post('My first Post', true),
+    new Post('My second Post', true),
+    new Post('My third Post', false),
+    new Post('My fourth Post', true)
+];
 
-// print_r($a);
-// print_r($b);
-// echo $a[1];
-// echo $b;
+
+$unPublishedPosts = array_filter($posts, function($post) {
+    return ! $post->published;
+});
+
+// die(var_dump($unPublishedPosts));
+
+$publishedPosts = array_filter($posts, function($post) {
+    return $post->published;
+});
+
+die(var_dump($publishedPosts));
+
